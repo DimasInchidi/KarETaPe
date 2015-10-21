d6e39bd8ae212cc23ab001da7a290599c856b0b4
DimasInchidi <inchidi@yahoo.com>
2015-10-22 00:22:08 +0800

    db connection set

--- src/main/java/servlet/DB_Transaction.java
+++ src/main/java/servlet/DB_Transaction.java
@@ -21,7 +21,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import javax.swing.JOptionPane;
 
 /**
  *
@@ -29,10 +28,10 @@ import javax.swing.JOptionPane;
  */
 public class DB_Transaction {
 
-    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    private static String DB_URL = "jdbc:postgresql://ec2-54-204-15-48.compute-1.amazonaws.com:5432/heketonDB";
-    private static final String USER = "heketonRoot";
-    private static String PASS = "janganlihat";
+    private static final String JDBC_DRIVER = "org.postgresql.Driver";
+    private static final String DB_URL = "jdbc:postgresql://ec2-54-204-15-48.compute-1.amazonaws.com:5432/d43cvp6kpf1the";
+    private static final String USER = "khwivmymedwodp";
+    private static final String PASS = "rnNyTeGxo0PCll83A-6Db__n4v";
     private ResultSet rs;
     private Connection con;
     Statement stmt;
@@ -42,29 +41,9 @@ public class DB_Transaction {
             Class.forName(JDBC_DRIVER);
             con = DriverManager.getConnection(DB_URL, USER, PASS);
         } catch (SQLException se) {
-            JOptionPane.showMessageDialog(null, "Tidak dapat terhubung ke database\n" + se.getMessage(), "Kesalahan Koneksi", JOptionPane.ERROR_MESSAGE);
-            if (se.getMessage().contains("denied")) {
-                PASS = JOptionPane.showInputDialog("Coba dengan password berbeda\nMasukan password");
-                try {
-                    con = DriverManager.getConnection(DB_URL, USER, PASS);
-                    stmt = con.createStatement();
-                } catch (SQLException se2) {
-                    System.exit(0);
-                }
-            }
-            if (se.getMessage().contains("database")) {
-                DB_URL = "jdbc:mysql://localhost/" + JOptionPane.showInputDialog("Masukan database yang digunakan");
-                try {
-                    con = DriverManager.getConnection(DB_URL, USER, PASS);
-                    stmt = con.createStatement();
-                } catch (SQLException se2) {
-                    System.exit(0);
-                }
-            }
-            System.exit(0);
+            System.out.println("Tidak dapat terhubung ke database\n" + se.getMessage());
         } catch (ClassNotFoundException e) {
-            JOptionPane.showMessageDialog(null, "Koneksi tidak valid\n" + e.getMessage(), "Kesalahan", JOptionPane.ERROR_MESSAGE);
-            System.exit(0);
+            System.out.println("Koneksi tidak valid\n" + e.getMessage());
         }
         return con;
     }
